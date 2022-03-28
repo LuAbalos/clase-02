cc33ab779ff9c82f97bd4af7af2bb1bd68ea5a45
Lu Abalos <abaloslucia@gmail.com>
2022-03-28 18:25:45 -0300

    Initial commit

---
+++ src/components/navBar.js
@@ -0,0 +1,31 @@
+const navBar = () => {
+    return (
+        <nav>
+            <ul class="navegador">         
+                <li>
+                     <span> Home </span>
+                </li>
+                
+                <li>
+                   <span style="color:black;  text-decoration: none;"> Servicios </span>
+                </li>
+                
+                <li>
+                   <span style="color:black; text-decoration: none;"> Productos </span>
+                </li>
+                
+                <li>
+                    <span style="color:black;text-decoration: none"> Contacto</span>
+                </li>
+                
+                <li>
+                    <span style="color:black; text-decoration: none"> Quienes somos</span>
+                </li>
+            </ul>
+        </nav>
+
+
+    );
+}
+
+export default navBar;
